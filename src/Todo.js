f4cb8109fda6b546223f8f138f7e1a1c3a025982
ruben <174733720+ruben-23@users.noreply.github.com>
2025-09-09 19:21:00 +0300

    Add projectId property and toggleChecked methid for Todo class

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,18 @@
 
 export default class Todo {
 
-    constructor(title, description, dueDate, priority, notes, checked) {
+    constructor(title, description, dueDate, priority, projectId, notes) {
         this.id = crypto.randomUUID();
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.projectId = projectId,
         this.notes = notes;
-        this.checked = checked;
+        this.checked = false;
     }
 
+    toggleChecked() {
+        this.checked === true ? this.checked = false : this.checked = true;
+    }
 }
