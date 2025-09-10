4a6550890b6f0be203cb44580be9ba51fd79be27
ruben <174733720+ruben-23@users.noreply.github.com>
2025-09-10 17:11:14 +0300

    Add function to display project cards

--- src/displayProjects.js
+++ src/displayProjects.js
@@ -1,5 +1,42 @@
 
+import displayAddProjectModal from './addProjectModal';
 import './displayProjects.css';
+import displayTodo from './displayTodos';
+import { getProjectById } from './projectsController';
+
+export function displayProjectCards(projects) {
+    
+    const contentItems = document.querySelector('.content-items');
+    contentItems.innerHTML = '';
+    
+    for (let project of projects) {
+        const projectCard = document.createElement('div');
+        projectCard.classList.add('project-card');
+
+        projectCard.setAttribute('data-id', project.id);
+
+        const title = document.createElement('h2');
+        title.textContent = project.title;
+
+        const description = document.createElement('p');
+        description.textContent = project.description;
+        
+        const todoCount = document.createElement('p');
+        todoCount.textContent = `No. of todos: ${project.getTodoCount()}`;
+
+        const cardButtons = document.createElement('div');
+        cardButtons.classList.add('project-card-buttons');
+
+        const viewTodoButton = document.createElement('button');
+        viewTodoButton.textContent = 'View todos'
+
+        cardButtons.appendChild(viewTodoButton);
+        projectCard.append(title, description, todoCount, cardButtons);
+        contentItems.appendChild(projectCard);
+
+        viewTodoButton.addEventListener('click', () => displayTodo([getProjectById(project.id)]));
+    }
+}
 
 export default function displayProjects(projects) {
 
@@ -24,35 +61,10 @@ export default function displayProjects(projects) {
     const contentItems = document.createElement('div');
     contentItems.classList.add('content-items');
 
-    for (let project of projects) {
-        const projectCard = document.createElement('div');
-        projectCard.classList.add('project-card');
-
-        projectCard.setAttribute('data-id', project.id);
-
-        const title = document.createElement('h2');
-        title.textContent = project.title;
-
-        const description = document.createElement('p');
-        description.textContent = project.description;
-        
-        const taskCount = document.createElement('p');
-        taskCount.textContent = `No. of tasks: ${project.getTaskCount()}`;
-
-        const cardButtons = document.createElement('div');
-        cardButtons.classList.add('project-card-buttons');
-
-        const viewTasksButton = document.createElement('button');
-        viewTasksButton.textContent = 'View tasks'
-
-        cardButtons.appendChild(viewTasksButton);
-
-        projectCard.append(title, description, taskCount, cardButtons);
-
-        contentItems.appendChild(projectCard);
-    }
-
     content.append(buttonsDiv, contentItems);
     mainContainer.appendChild(content);
 
+    displayProjectCards(projects);
+
+    addButton.addEventListener('click', () => { displayAddProjectModal() });
 }
